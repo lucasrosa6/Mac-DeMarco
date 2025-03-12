66e44016a145bdff1e3ace70726218fd44e8add2
Lucas Do Nascimento Rosa <143842123+lucasrosa6@users.noreply.github.com>
2025-03-12 16:36:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
+
 
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
@@ -9,20 +10,47 @@ document.addEventListener('DOMContentLoaded', function(){
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
 
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+
     })
+
+
+
+
+
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function(){
+
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+
     })
-    diminuiFonteBotao.addEventListener('click', function(){
+
+
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+
     })
+
+
+   alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+     })
+    
+
 
 })
