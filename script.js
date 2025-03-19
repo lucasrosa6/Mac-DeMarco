3febe99333c2266cb596df621f00493649e6226b
Lucas Do Nascimento Rosa <143842123+lucasrosa6@users.noreply.github.com>
2025-03-19 16:15:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -54,3 +54,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
